d98678a486cdfb390261cf189d99728f68c12a9a
git <kiarash.edraki@gmail.com>
2018-09-12 21:53:30 -0700

    updated gradle to work with war and tomcat

--- src/main/java/me/edraki/Application.java
+++ src/main/java/me/edraki/Application.java
@@ -1,19 +1,22 @@
 package me.edraki;
 
-import me.edraki.service.PropertyReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableAutoConfiguration()
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-        System.out.println(context.containsBean("propertyReader"));
-        PropertyReader propertyReader =  (PropertyReader) context.getBean("propertyReader");
-        System.out.println(propertyReader.mangoDbHost);
     }
 }
